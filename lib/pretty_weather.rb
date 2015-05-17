0db392cf2372f3c2f4b4e0225bb91ec479220236
nikitasmall <nikitasosnov@yahoo.com>
2015-05-17 21:11:42 +0300

    icon_tag implemented

--- lib/pretty_weather.rb
+++ lib/pretty_weather.rb
@@ -3,10 +3,10 @@ require 'nokogiri'
 
 module PrettyWeather
   class PrettyWeather
-    attr_accessor :temp, :weather
+    attr_accessor :temp, :weather, :icon_tag
 
     def initialize(city_name)
-      link = "http://api.openweathermap.org/data/2.5/weather?q=#{city_name}&mode=xml&units=metric" #Odesa
+      link = "http://api.openweathermap.org/data/2.5/weather?q=#{city_name}&mode=xml&units=metric" #Odesa for Odessa in Ukraine
       attempts = 3
 
       begin
@@ -17,6 +17,8 @@ module PrettyWeather
 
         @temp = "+#{@temp}" unless @temp.index('-')
         @weather = weather_name(weather_code)
+
+        @icon_tag = select_weather_icon @weather
       rescue OpenURI::HTTPError => e
         attempts -= 1
         retry unless attempts.zero?
@@ -50,6 +52,40 @@ module PrettyWeather
         else
           'good'
       end
+    end
+
+    def select_weather_icon(weather)
+      icon = case weather
+               when 'sunny'
+                 '<i class="wi wi-day-sunny"></i>'
+               when 'moonly'
+                 '<i class="wi wi-night-clear"></i>'
+               when 'day_cloud'
+                 '<i class="wi wi-day-cloudy"></i>'
+               when 'night_cloud'
+                 '<i class="wi wi-night-alt-cloudy"></i>'
+               when 'foggy'
+                 '<i class="wi wi-dust"></i>'
+               when 'day_rain'
+                 '<i class="wi wi-day-showers"></i>'
+               when 'night_rain'
+                 '<i class="wi wi-night-alt-sprinkle"></i>'
+               when 'heavy_rain'
+                 '<i class="wi wi-sprinkle"></i>'
+               when 'windy'
+                 '<i class="wi wi-cloudy-gusts"></i>'
+               when 'snow'
+                 '<i class="wi wi-snow"></i>'
+               when 'thunder'
+                 '<i class="wi wi-thunderstorm"></i>'
+               when 'cloud'
+                 '<i class="wi wi-cloudy"></i>'
+               when 'good'
+                 '<i class="wi wi-thermometer-exterior"></i>'
+               else
+                 '<i class="wi wi-cloud-refresh"></i>'
+             end
+      icon.html_safe
     end
   end
 end
--- test/pretty_weather_test.rb
+++ test/pretty_weather_test.rb
@@ -12,4 +12,15 @@ class PrettyWeatherTest < ActiveSupport::TestCase
   test "weather_temp_should_be_a_number" do
     assert_kind_of String, @weather.temp
   end
+
+  test "weather_state_should_be_not_less_than_four_length_string" do
+    assert_kind_of String, @weather.weather
+    assert @weather.weather.length >= 4
+  end
+
+  test "weather_icon_should_be_a_string_with_tag_i" do
+    assert_kind_of String, @weather.icon_tag
+    assert @weather.icon_tag.index('<i') >= 0
+    assert @weather.icon_tag.index('</i>') >= 0
+  end
 end
