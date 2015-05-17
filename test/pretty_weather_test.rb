4e52e27e3c541118131a019b14711601d02e0608
nikitasmall <nikitasosnov@yahoo.com>
2015-05-17 18:48:13 +0300

    added initialization

--- test/pretty_weather_test.rb
+++ test/pretty_weather_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class PrettyWeatherTest < ActiveSupport::TestCase
+  setup do
+    @weather = PrettyWeather::PrettyWeather.new('odesa')
+  end
+
   test "truth" do
     assert_kind_of Module, PrettyWeather
+  end
+
+  test "weather_temp_should_be_a_number" do
+    assert_kind_of String, @weather.temp
   end
 end
