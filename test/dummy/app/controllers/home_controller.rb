9cd53745061f897c949359ae25b0fbec95f8a7a8
nikitasmall <nikitasosnov@yahoo.com>
2015-05-18 10:52:21 +0300

    deleted dummy web-app

--- test/dummy/app/controllers/home_controller.rb
+++
@@ -1,5 +0,0 @@
-class HomeController < ApplicationController
-  def index
-    @weather = PrettyWeather::Weather.new('odesa', 'metric')
-  end
-end
--- test/dummy/config/initializers/pretty_weather.rb
+++
@@ -1,4 +0,0 @@
-Rails.application.config.pretty_weather.set_options do |options|
-  options.city_name = 'yakutsk'
-  options.units = 'imperial'
-end
